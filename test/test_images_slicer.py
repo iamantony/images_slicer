7b3f611b63c79f26e9455c3aedfb98c018b0ac7b
an.cherepanov <an.cherepanov@tensor.ru>
2014-09-08 15:07:26 +0600

    Tests for check_arguments()

--- test/test_images_slicer.py
+++ test/test_images_slicer.py
@@ -1,15 +1,41 @@
 __author__ = 'Antony Cherepanov'
 
 import unittest
+import os
+import shutil
+import images_slicer
 
 
-class FakeTest(unittest.TestCase):
-    def setUp(self):
-        print("!-- In setUp()")
+SCRIPT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+TEST_SAVE_FOLDER = SCRIPT_FOLDER + "test_folder"
 
-    def test_fake(self):
-        print("!-- In test_fake()")
 
+class CheckArgumentsTest(unittest.TestCase):
+    def test_valid_folder(self):
+        self.assertTrue(images_slicer.check_arguments(SCRIPT_FOLDER, 1, 1, ""))
+
+    def test_invalid_folder(self):
+        self.assertFalse(images_slicer.check_arguments("/invalid/folder", 1, 1, ""))
+
+    def test_invalid_width(self):
+        self.assertTrue(images_slicer.check_arguments(SCRIPT_FOLDER, -10, 1, ""))
+
+    def test_invalid_height(self):
+        self.assertTrue(images_slicer.check_arguments(SCRIPT_FOLDER, 1, 0, ""))
+
+    def test_invalid_save_folder(self):
+        self.assertTrue(images_slicer.check_arguments(SCRIPT_FOLDER, 1, 0, "../fake/"))
+
+    def test_save_folder_creation(self):
+        self.assertTrue(images_slicer.check_arguments(SCRIPT_FOLDER, 1, 0, TEST_SAVE_FOLDER))
+
+    def tearDown(self):
+        if os.path.exists(TEST_SAVE_FOLDER):
+            try:
+                shutil.rmtree(TEST_SAVE_FOLDER, True)
+            except Exception as err:
+                print("Error during folder remove: {0}".format(err))
+                return
 
 if __name__ == "__main__":
     unittest.main()
